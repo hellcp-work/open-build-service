e20f74e8f3f6eb0dcd0245d3048c5f23606bf191
Jacob Michalskie <jacob.michalskie@suse.com>
2022-12-22 16:13:46 +0100

    Diff viewer poc

--- src/api/app/components/sourcediff_component.rb
+++ src/api/app/components/sourcediff_component.rb
@@ -9,8 +9,58 @@ class SourcediffComponent < ApplicationComponent
 
     @bs_request = bs_request
     @action = action
+
+    action[:sourcediff].each do |sourcediff|
+      if sourcediff['filenames'].present?
+        sourcediff['filenames'].each_with_index do |filename, file_index|
+          content = sourcediff['files'][filename].dig('diff', '_content')
+          original_index = 0
+          changed_index = 0
+          last_original_index = 0
+          last_changed_index = 0
+          lines = []
+          content&.each_line&.with_index do |line, index|
+            range = false
+            state = 'unknown'
+            case line
+            when /^@@ -(?<original_index>\d+).+\+(?<changed_index>\d+),/
+              original_index = Regexp.last_match[:original_index].to_i - 1
+              changed_index = Regexp.last_match[:changed_index].to_i - 1
+              state = 'range'
+            when GitDiffParser::Patch::REMOVED_LINE
+              original_index += 1
+              changed_index += 0
+              state = 'removed'
+            when /^[+]/
+              original_index += 0
+              changed_index += 1
+              state = 'added'
+            else
+              original_index += 1
+              changed_index += 1
+              state = 'unchanged'
+            end
+            line_hash = { content: line[1..-1], state: }
+            line_hash[:original_index] = original_index unless last_original_index == original_index
+            line_hash[:changed_index] = changed_index unless last_changed_index == changed_index
+            line_hash[:last_original_index] = last_original_index if state == 'range'
+            line_hash[:last_changed_index] = last_changed_index if state == 'range'
+            line_hash[:index] = index + 1
+            lines << line_hash unless range
+            last_original_index = original_index
+            last_changed_index = changed_index
+          end
+          sourcediff['files'][filename]['diff']['hash'] = lines
+        end
+      end
+    end
     @index = index
     @refresh = refresh
+  end
+
+  def offset(num)
+    off = 3 - num.to_s.length
+    (' ' * off) + num.to_s
   end
 
   def file_view_path(filename, sourcediff)
