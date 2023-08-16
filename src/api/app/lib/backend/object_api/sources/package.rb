f221e06c9cd0f8de6392c8a94ed6af7ccaeaa181
Jacob Michalskie <jacob.michalskie@suse.com>
2023-08-16 14:32:36 +0200

    Introduce ObjectApi

---
+++ src/api/app/lib/backend/object_api/sources/package.rb
@@ -0,0 +1,17 @@
+module Backend
+  module ObjectApi
+    module Sources
+      # Class that connect to endpoints related to source packages
+      module Package
+        extend Backend::ConnectionHelper
+
+        # Returns a file list of the sources for a package
+        # @param options [Hash] Parameters to pass to the backend.
+        # @return [String]
+        def self.files(project_name, package_name, options = {})
+          Backend::Responses::Directory.new(http_get(['/source/:project/:package', project_name, package_name], params: options, accepted: [:expand, :rev, :view]))
+        end
+      end
+    end
+  end
+end
