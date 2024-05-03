3a41d0d80856a8f55e64554df3b2c4ed4eef97ab
Jacob Michalskie <jacob.michalskie@suse.com>
2024-05-03 13:33:31 +0200

    Replace badge use of LocalBuildResult

--- src/api/app/controllers/webui/packages/badge_controller.rb
+++ src/api/app/controllers/webui/packages/badge_controller.rb
@@ -5,18 +5,19 @@ module Webui
       before_action :set_package
 
       def show
-        results = @package.buildresult(@project, false, true).results[@package.name]
-        results = results.select { |r| r.architecture == params[:architecture] } if params[:architecture]
-        results = results.select { |r| r.repository == params[:repository] } if params[:repository]
-        results = discard_non_relevant_results(results) unless results.nil?
-        badge = Badge.new(params[:type], results)
+        opts = { package: @package.name, lastbuild: 1, arch: params[:architecture], repository: params[:repository] }.compact
+        results = Backend::Models::Resultlist.fetch(@project.name, opts).results
+        results = discard_non_relevant_results(results)
+        badge = Badge.new(params[:type], results.map { |r| r.statuses }.flatten)
         send_data(badge.xml, type: 'image/svg+xml', disposition: 'inline')
       end
+
+      private
 
       # discard results with excluded and disabled status
       # discard disabled with possible previous failed results
       def discard_non_relevant_results(results)
-        results.reject { |r| Buildresult.new(r.code).refused_status? || @package.disabled_for?('build', r.repository, r.architecture) }
+        results.reject { |r| @package.disabled_for?('build', r.repository, r.arch) }
       end
     end
   end
